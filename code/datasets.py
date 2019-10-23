d66419a802b0541e39edbdb7ebd40475ba29716d
SeitaroShinagawa <shinagwa.seitaro.si8@is.naist.jp>
2019-10-23 15:03:39 +0900

    modify dataset

--- code/datasets.py
+++ code/datasets.py
@@ -25,20 +25,34 @@ else:
     import pickle
 
 
-def prepare_data(data):
-    imgs, captions, captions_lens, class_ids, keys = data
+def prepare_data(data, use_attribute=False, use_embedding=False):
+    imgs, captions, captions_lens, class_ids, keys = data[:5]
+    if use_attribute and use_embedding:
+        assert len(data[5:]) == 2
+        embeddings, attributes = data[5:]
+    elif use_attribute:
+        assert len(data[5:]) == 1
+        attributes = data[5]
+    elif use_embedding:
+        assert len(data[5:]) == 1
+        embeddings = data[5]
 
     # sort data by the length in a decreasing order
     sorted_cap_lens, sorted_cap_indices = \
         torch.sort(captions_lens, 0, True)
 
     real_imgs = []
+    wrong_imgs = []
+    wrong_indices = torch.randperm(len(sorted_cap_indices))
     for i in range(len(imgs)):
-        imgs[i] = imgs[i][sorted_cap_indices]
+        r_imgs = imgs[i][sorted_cap_indices] #sort by caption length order
+        w_imgs = imgs[i][wrong_indices] #sort by caption length order
         if cfg.CUDA:
-            real_imgs.append(Variable(imgs[i]).cuda())
+            real_imgs.append(Variable(r_imgs).cuda())
+            wrong_imgs.append(Variable(w_imgs).cuda())
         else:
-            real_imgs.append(Variable(imgs[i]))
+            real_imgs.append(Variable(r_imgs))
+            wrong_imgs.append(Variable(w_imgs))
 
     captions = captions[sorted_cap_indices].squeeze()
     class_ids = class_ids[sorted_cap_indices].numpy()
@@ -48,12 +62,25 @@ def prepare_data(data):
     if cfg.CUDA:
         captions = Variable(captions).cuda()
         sorted_cap_lens = Variable(sorted_cap_lens).cuda()
+        if use_embedding:
+            embeddings = embeddings[sorted_cap_indices].cuda()
+        if use_attribute:
+            attributes = attributes[sorted_cap_indices].float().cuda()
     else:
         captions = Variable(captions)
         sorted_cap_lens = Variable(sorted_cap_lens)
-
-    return [real_imgs, captions, sorted_cap_lens,
-            class_ids, keys]
+        if use_embedding:
+            embeddings = embeddings[sorted_cap_indices]
+        if use_attribute:
+            attributes = attributes[sorted_cap_indices].float()
+    
+    out_list = [real_imgs, wrong_imgs, captions, sorted_cap_lens, class_ids, keys]
+    if use_attribute:
+        out_list.append(attributes)
+    if use_embedding:
+        out_list.append(embeddings)
+    
+    return out_list
 
 
 def get_imgs(img_path, imsize, bbox=None,
@@ -80,6 +107,7 @@ def get_imgs(img_path, imsize, bbox=None,
         for i in range(cfg.TREE.BRANCH_NUM):
             # print(imsize[i])
             if i < (cfg.TREE.BRANCH_NUM - 1):
+                #re_img = transforms.Scale(imsize[i])(img)
                 re_img = transforms.Resize(imsize[i])(img)
             else:
                 re_img = img
@@ -91,13 +119,15 @@ def get_imgs(img_path, imsize, bbox=None,
 class TextDataset(data.Dataset):
     def __init__(self, data_dir, split='train',
                  base_size=64,
-                 transform=None, target_transform=None):
+                 transform=None, target_transform=None,
+                 use_attribute=False, use_embedding=False):
         self.transform = transform
         self.norm = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         self.target_transform = target_transform
         self.embeddings_num = cfg.TEXT.CAPTIONS_PER_IMAGE
+        self.specific_sent_ix = None
 
         self.imsize = []
         for i in range(cfg.TREE.BRANCH_NUM):
@@ -106,38 +136,61 @@ class TextDataset(data.Dataset):
 
         self.data = []
         self.data_dir = data_dir
+        all_filenames = self.load_all_filenames()
         if data_dir.find('birds') != -1:
-            self.bbox = self.load_bbox()
+            self.bbox = self.load_bbox(all_filenames)
         else:
             self.bbox = None
-        split_dir = os.path.join(data_dir, split)
+        
+        if self.use_attribute:
+            self.attributes, self.all_filename_id_dict = self.load_att(all_filenames)
+            #self.att_conf = self.load_att_conf()
+
+        self.split_dir = os.path.join(data_dir, split)
 
         self.filenames, self.captions, self.ixtoword, \
             self.wordtoix, self.n_words = self.load_text_data(data_dir, split)
 
-        self.class_id = self.load_class_id(split_dir, len(self.filenames))
+        if self.use_embedding:
+            self.embeddings = self.load_embedding(self.split_dir, 'myembedding')
+
+        self.class_id = self.load_class_id(self.split_dir, len(self.filenames))
         self.number_example = len(self.filenames)
 
-    def load_bbox(self):
+    def load_all_filenames(self):
+        data_dir = self.data_dir
+        filepath = os.path.join(data_dir, 'CUB_200_2011/images.txt')
+        df_filenames = \
+            pd.read_csv(filepath, delim_whitespace=True, header=None)
+        all_filenames = df_filenames[1].tolist()
+        print('Total filenames: ', len(all_filenames), all_filenames[0])
+        return all_filenames
+
+    def load_att(self, all_filenames):
+        data_dir = self.data_dir
+        att_path = os.path.join(data_dir, 'CUB_200_2011/attributes/birds_att.npy')
+        all_filename_id_dict = {filename:index for index, filename in enumerate(all_filenames)}
+        return np.load(att_path), all_filename_id_dict
+    
+    def load_att_conf(self):
+        conf_path = 'birds_att_conf.npy'
+        conf_array = np.load(conf_path)
+        return conf_array.astype("f")
+
+    def load_bbox(self, all_filenames):
         data_dir = self.data_dir
         bbox_path = os.path.join(data_dir, 'CUB_200_2011/bounding_boxes.txt')
         df_bounding_boxes = pd.read_csv(bbox_path,
                                         delim_whitespace=True,
                                         header=None).astype(int)
-        #
-        filepath = os.path.join(data_dir, 'CUB_200_2011/images.txt')
-        df_filenames = \
-            pd.read_csv(filepath, delim_whitespace=True, header=None)
-        filenames = df_filenames[1].tolist()
-        print('Total filenames: ', len(filenames), filenames[0])
-        #
-        filename_bbox = {img_file[:-4]: [] for img_file in filenames}
-        numImgs = len(filenames)
+        
+        filename_bbox = {img_file[:-4]: [] for img_file in all_filenames}
+        numImgs = len(all_filenames)
         for i in range(0, numImgs):
             # bbox = [x-left, y-top, width, height]
             bbox = df_bounding_boxes.iloc[i][1:].tolist()
 
-            key = filenames[i][:-4]
+            key = all_filenames[i][:-4]
             filename_bbox[key] = bbox
         #
         return filename_bbox
@@ -146,8 +199,9 @@ class TextDataset(data.Dataset):
         all_captions = []
         for i in range(len(filenames)):
             cap_path = '%s/text/%s.txt' % (data_dir, filenames[i])
-            with open(cap_path, "r") as f:
-                captions = f.read().decode('utf8').split('\n')
+            with open(cap_path, "r", encoding="latin1") as f:
+                #captions = f.read().decode('utf8').split('\n')
+                captions = f.read().split('\n')
                 cnt = 0
                 for cap in captions:
                     if len(cap) == 0:
@@ -248,6 +302,21 @@ class TextDataset(data.Dataset):
             filenames = test_names
         return filenames, captions, ixtoword, wordtoix, n_words
 
+    def specify_caption_index(index):
+        self.specific_sent_ix = index
+        print("specified sentence index = ", self.specific_sent_ix)
+    
+    def load_embedding(self, data_dir, embedding_type='myembedding'):
+        if embedding_type == 'myembedding':
+            embedding_filename = '/Myembedding.pickle'
+
+        with open(data_dir + embedding_filename, 'rb') as f:
+            embeddings = pickle.load(f)
+            #embeddings = pickle.load(f, encoding="latin1")
+            print('embeddings loaded')
+        return embeddings
+
+
     def load_class_id(self, data_dir, total_num):
         if os.path.isfile(data_dir + '/class_info.pickle'):
             with open(data_dir + '/class_info.pickle', 'rb') as f:
@@ -286,27 +355,143 @@ class TextDataset(data.Dataset):
             x_len = cfg.TEXT.WORDS_NUM
         return x, x_len
 
+
     def __getitem__(self, index):
-        #
+        
         key = self.filenames[index]
         cls_id = self.class_id[index]
-        #
         if self.bbox is not None:
             bbox = self.bbox[key]
             data_dir = '%s/CUB_200_2011' % self.data_dir
         else:
             bbox = None
             data_dir = self.data_dir
-        #
+        
+        # Get image
         img_name = '%s/images/%s.jpg' % (data_dir, key)
         imgs = get_imgs(img_name, self.imsize,
                         bbox, self.transform, normalize=self.norm)
-        # random select a sentence
-        sent_ix = random.randint(0, self.embeddings_num)
+
+        # Select a sentence
+        if self.specific_sent_ix is not None:
+            sent_ix = self.specific_sent_ix 
+        else:
+            sent_ix = random.randint(0, self.embeddings_num)
+        
         new_sent_ix = index * self.embeddings_num + sent_ix
         caps, cap_len = self.get_caption(new_sent_ix)
-        return imgs, caps, cap_len, cls_id, key
+
+
+        if self.use_attribute:
+            attribute = self.attributes[self.all_filename_id_dict[key+".jpg"]] # (1,312) array
+            #att_conf = self.att_conf[self.all_filename_id_dict[key+".jpg"]]
+
+        if self.use_embedding: 
+            embedding = self.embeddings[index, sent_ix, :]
+        
+        if self.use_attribute and self.use_embedding: 
+            return imgs, caps, cap_len, cls_id, key, attribute, embedding
+        elif self.use_attribute: 
+            return imgs, caps, cap_len, cls_id, key, attribute
+        elif self.use_embedding: 
+            return imgs, caps, cap_len, cls_id, key, embedding
+        else:
+            return imgs, caps, cap_len, cls_id, key
 
 
     def __len__(self):
         return len(self.filenames)
+
+
+if __name__ == "__main__":
+    # python datasets.py --cfg cfg/bird_embedding.yml
+    from main import parse_args
+    args = parse_args()
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
+
+    if args.gpu_id != -1:
+        cfg.GPU_ID = args.gpu_id
+    else:
+        cfg.CUDA = False
+
+    if args.data_dir != '':
+        cfg.DATA_DIR = args.data_dir
+
+    args.manualSeed = 100
+    random.seed(args.manualSeed)
+    np.random.seed(args.manualSeed)
+    torch.manual_seed(args.manualSeed)
+    if cfg.CUDA:
+        torch.cuda.manual_seed_all(args.manualSeed)
+
+    imsize = cfg.TREE.BASE_SIZE * (2 ** (cfg.TREE.BRANCH_NUM - 1))
+    image_transform = transforms.Compose([
+        transforms.Resize(int(imsize * 76 / 64)),
+        transforms.RandomCrop(imsize),
+        transforms.RandomHorizontalFlip()])
+    
+    dataset_train = TextDataset(cfg.DATA_DIR, 'train',
+                          base_size=cfg.TREE.BASE_SIZE,
+                          transform=image_transform)
+    assert dataset_train
+    dataset_test = TextDataset(cfg.DATA_DIR, 'test',
+                          base_size=cfg.TREE.BASE_SIZE,
+                          transform=image_transform)
+    assert dataset_test
+
+    dataloader_train = torch.utils.data.DataLoader(
+        dataset_train, batch_size=cfg.TRAIN.BATCH_SIZE,
+        drop_last=False, shuffle=False, num_workers=int(cfg.WORKERS))
+    
+    dataloader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=cfg.TRAIN.BATCH_SIZE,
+        drop_last=False, shuffle=False, num_workers=int(cfg.WORKERS))
+
+	# Build text encoder    
+    text_encoder = \
+        RNN_ENCODER(n_words, nhidden=cfg.TEXT.EMBEDDING_DIM)
+    state_dict = \
+        torch.load(cfg.TRAIN.NET_E,
+                   map_location=lambda storage, loc: storage) #load text encoder
+    text_encoder.load_state_dict(state_dict)
+    for p in text_encoder.parameters():
+        p.requires_grad = False
+    print('Load text encoder from:', cfg.TRAIN.NET_E)
+    text_encoder.eval()
+
+    if cfg.CUDA:
+        text_encoder = text_encoder.cuda()
+
+	def create_embeddings(dataloader, text_encoder):
+		emb_dict = {}
+        for sent_ix in range(cfg.TEXT.CAPTIONS_PER_IMAGE):
+    	    dataloader.dataset.specify_caption_index(sent_ix) 
+            for data in dataloader:
+			    real_imgs, wrong_imgs, captions, sorted_cap_lens,
+				    class_ids, keys = prepare_data(data)
+
+                hidden = text_encoder.init_hidden(batch_size) 
+                # words_embs: batch_size x nef x seq_len
+                # sent_emb: batch_size x nef
+                words_embs, sent_emb = text_encoder(captions, cap_lens, hidden)
+                words_embs, sent_emb = words_embs.detach(), sent_emb.detach()
+
+                for i, key in enumerate(keys):
+                    emb_elements = { "words": words_embs[i],
+                                    "sentence": sent_emb[i],
+                                    "cap_len": sorted_cap_lens[i] }
+                    if key not in emb_dict:
+                        emb_dict[key] = [emb_elements]
+                    else:
+                        emb_dict[key].append(emb_elements)
+		return emb_dict
+
+	train_emb_dict = create_embeddings(dataloader_train, text_encoder) 
+	test_emb_dict = create_embeddings(dataloader_test, text_encoder)
+    def save_emb(path, emb_dict):
+        with open(path, "wb") as f:
+            pickle.dump(emb_dict, f)
+
+    save_emb("embedding_train.pickle", train_emb_dict)
+    save_emb("embedding_test.pickle", test_emb_dict)
